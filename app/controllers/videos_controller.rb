cbfa3ff7bbe4da5893b07362ba4d2f6c3e4f001f
Gareth Connop <g.connop@btinternet.com>
2015-03-24 15:53:26 +0000

    Add application gems and video view files

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,22 @@
 class VideosController < ApplicationController
   def index
-  	
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
   end
 end
