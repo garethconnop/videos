668b22114f1fe8a9459334f7e727c9a09cc09ad1
Gareth Connop <g.connop@btinternet.com>
2015-03-24 16:58:44 +0000

    Add create update destroy ability for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,22 +1,51 @@
 class VideosController < ApplicationController
-  def index
-  end
+  before_action :find_video, only: [:show, :edit, :update, :destroy]
 
-  def new
+  def index
+  	@videos = Video.all.order("created_at DESC")
   end
 
   def show
+  	@video = Video.find(params[:id])
+  end
+
+  def new
+  	@video = Video.new
   end
 
   def create
-  end
+  	@video = Video.new(video_params)
 
-  def update
+  	if @video.save
+  	  redirect_to @video
+  	else
+  	  render 'new'
+  	end
   end
 
   def edit
   end
 
+  def update
+  	if @video.update(video_params)
+  	  redirect_to @video
+  	else
+  	  render 'edit'
+  	end
+  end
+
   def destroy
+  	@video.destroy
+  	redirect_to root_path
+  end
+
+  private
+
+  def find_video
+  	@video = Video.find(params[:id])
+  end
+
+  def video_params
+  	params.require(:video).permit(:title, :length, :date, :description, :video_id, :slug)
   end
 end
